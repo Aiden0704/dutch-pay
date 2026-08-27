3ae3f9b37509e28ac112b7f7f375a0bbda06feed
Aiden0704 <wnsqhs7804@gmail.com>
2026-08-27 21:16:28 +0900

    fix: eslint 설정에 @typescript-eslint 플러그인 미등록 오류 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'eslint/config';
+import tseslintPlugin from '@typescript-eslint/eslint-plugin';
+import tseslintParser from '@typescript-eslint/parser';
 
 export default defineConfig([
   {
     files: ['**/*.ts'],
+    languageOptions: {
+      parser: tseslintParser,
+    },
+    plugins: {
+      '@typescript-eslint': tseslintPlugin,
+    },
     rules: {
       'no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
