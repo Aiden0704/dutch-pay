e9c8994eb5dc25a48031c374bc2ddba98c7d644e
Aiden0704 <wnsqhs7804@gmail.com>
2026-08-27 21:19:51 +0900

    feat: 로그인 화면 UI 구현

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
 import './style.css';
+import { renderLogin } from './login/ui/login';
+
+const app = document.querySelector<HTMLDivElement>('#app');
+if (!app) {
+  throw new Error('#app 엘리먼트를 찾을 수 없습니다');
+}
+
+renderLogin(app);
