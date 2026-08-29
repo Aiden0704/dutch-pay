b1eb0fbcdf76b276d82340c5ca4b91b97206a0ea
Aiden0704 <wnsqhs7804@gmail.com>
2026-08-29 22:17:14 +0900

    feat: Env에 SessionEnv 확장 추가

--- functions/api/kakao/types.ts
+++ functions/api/kakao/types.ts
@@ -1,6 +1,7 @@
+import type { SessionEnv } from '../../_shared/session';
 import type { SupabaseEnv } from '../../_shared/supabase';
 
-export interface Env extends SupabaseEnv {
+export interface Env extends SupabaseEnv, SessionEnv {
   KAKAO_REST_API_KEY: string;
   KAKAO_CLIENT_SECRET: string;
   KAKAO_REDIRECT_URI: string;
