890f84e32a77e48cafc1c269d76476015a509b28
Aiden0704 <wnsqhs7804@gmail.com>
2026-08-29 21:48:04 +0900

    refactor: 콜백 핸들러가 분리된 함수들을 조율하도록 정리

--- functions/api/kakao/callback.ts
+++ functions/api/kakao/callback.ts
@@ -1,23 +1,7 @@
 import type { Env } from './types';
-
-async function exchangeCodeForToken(code: string, env: Env) {
-  const url = 'https://kauth.kakao.com/oauth/token';
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-    },
-    body: new URLSearchParams({
-      grant_type: 'authorization_code',
-      client_id: env.KAKAO_REST_API_KEY,
-      client_secret: env.KAKAO_CLIENT_SECRET,
-      redirect_uri: env.KAKAO_REDIRECT_URI,
-      code: code,
-    }),
-  });
-
-  return response;
-}
+import { exchangeCodeForKakaoUser } from './kakao-oauth';
+import { createSupabaseClient } from '../../_shared/supabase';
+import { findOrCreateUser } from './repository';
 
 export async function onRequestGet({
   request,
@@ -33,26 +17,9 @@ export async function onRequestGet({
     return new Response('코드가 유효하지 않습니다', { status: 400 });
   }
 
-  const kakaoUser = exchangeCodeForKakaoUser(code, env);
-  return Response.json(kakaoUser);
-}
+  const kakaoUser = await exchangeCodeForKakaoUser(code, env);
+  const supabase = createSupabaseClient(env);
+  const user = await findOrCreateUser(kakaoUser, supabase);
 
-async function getKakaoUserInfo(accessToken: string) {
-  const url = 'https://kapi.kakao.com/v2/user/me';
-  const response = await fetch(url, {
-    method: 'GET',
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
-    },
-  });
-  return response;
-}
-
-async function exchangeCodeForKakaoUser(code: string, env: Env) {
-  const tokenResponse = await exchangeCodeForToken(code, env);
-  const token = await tokenResponse.json();
-
-  const userInfoResponse = await getKakaoUserInfo(token.access_token);
-  return await userInfoResponse.json();
+  return Response.json(user);
 }
