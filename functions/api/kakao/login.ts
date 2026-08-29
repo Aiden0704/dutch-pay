45d778208f06c8ef90d8a251a3dd213f24f6eab3
Aiden0704 <wnsqhs7804@gmail.com>
2026-08-29 16:34:10 +0900

    feat: 카카오 로그인 리다이렉트 엔드포인트 구현

---
+++ functions/api/kakao/login.ts
@@ -0,0 +1,10 @@
+import type { Env } from './types';
+
+export function onRequestGet({ env }: { env: Env }) {
+  const restApiKey = env.KAKAO_REST_API_KEY;
+  const redirectUri = env.KAKAO_REDIRECT_URI;
+
+  const url = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${restApiKey}&redirect_uri=${redirectUri}`;
+
+  return Response.redirect(url, 302);
+}
