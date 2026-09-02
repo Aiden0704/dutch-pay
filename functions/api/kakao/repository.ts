6d94459a84d7e6e14a930e940c5dbc82c5ab73db
Aiden0704 <wnsqhs7804@gmail.com>
2026-09-02 17:10:05 +0900

    refactor: findUserById 함수를 공용 유저 조회 함수로 만들어 _shared/users.ts로 이동

--- functions/api/kakao/repository.ts
+++ functions/api/kakao/repository.ts
@@ -31,19 +31,3 @@ export async function findOrCreateUser(
     return data[0];
   }
 }
-
-export async function findUserById(
-  userId: number,
-  supabaseClient: SupabaseClient
-) {
-  const { data, error } = await supabaseClient
-    .from('users')
-    .select()
-    .eq('id', userId);
-
-  if (!data) {
-    throw new Error(`조회 요청에 실패하였습니다: ${JSON.stringify(error)}`);
-  }
-
-  return data[0];
-}
